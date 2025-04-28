7b2801ce04f86b109ff19ecddb431cf6b1a1f322
kush's <patelkushgara@gamil.com>
2025-04-28 22:25:05 +0530

    deployed on sepolia testnet

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,12 @@
-require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
+require("@nomiclabs/hardhat-ethers");
+require(dotenv).config();
 module.exports = {
-  solidity: "0.8.28",
+  solidity: "0.8.20",
+  networks: {
+    sepolia: {
+      url: process.env.INFURA_URL,
+      accounts: [`0x${process.env.PRIVATE_KEY}`],
+    }
+  },
 };
--- script/deploy.js
+++ script/deploy.js
@@ -1,15 +1,22 @@
+const { ethers } = require("hardhat");
+
 async function main() {
-	const [deployer] = await ethers.getSigners();
-	console.log('Deploying contracts with the account:', deployer.address);
-	const ERP = await ethers.getContractFactory('CollegeERP');
-	const token = await ERP.deploy();
-	await token.waitForDeployment(); 
-	console.log('Token address:', await token.getAddress());
+  // Get the deployer's account
+  const [deployer] = await ethers.getSigners();
+
+  console.log("Deploying contracts with the account:", deployer.address);
+
+  // Deploy the contract
+  const CollegeERP = await ethers.getContractFactory("CollegeERP");
+  const collegeERP = await CollegeERP.deploy();
+
+  console.log("CollegeERP contract deployed to:", collegeERP.address);
 }
 
+// Run the deployment script
 main()
-	.then(() => process.exit(0))
-	.catch((error) => {
-		console.error(error);
-		process.exit(1);
-	});
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
